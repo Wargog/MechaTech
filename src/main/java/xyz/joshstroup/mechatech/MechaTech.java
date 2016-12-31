11e96b3e1d5983704743e217967d2ec2f0a84696
Joshua Stroup <josh@joshstroup.xyz>
2016-12-31 15:28:42 -0500

    Initial Commit

---
+++ src/main/java/xyz/joshstroup/mechatech/MechaTech.java
@@ -0,0 +1,20 @@
+package xyz.joshstroup.mechatech;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MechaTech.MODID, version = MechaTech.VERSION)
+public class MechaTech
+{
+    public static final String MODID = "mechatech";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
