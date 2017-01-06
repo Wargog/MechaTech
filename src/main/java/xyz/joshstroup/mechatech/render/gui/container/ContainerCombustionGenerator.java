7e540e4cc7492ea3970b1a0359450ea4865e2147
Jacob Farley <redstonedudeiscool@gmail.com>
2017-01-06 14:33:37 -0600

    Fix weird formatting of transferStackInSlot

--- src/main/java/xyz/joshstroup/mechatech/render/gui/container/ContainerCombustionGenerator.java
+++ src/main/java/xyz/joshstroup/mechatech/render/gui/container/ContainerCombustionGenerator.java
@@ -29,14 +29,12 @@ public class ContainerCombustionGenerator extends MechaTechContainer<TileEntityC
     	ItemStack itemstack = null;
     	Slot slot = (Slot)this.inventorySlots.get(index);
     	
-    	if (slot != null && slot.getHasStack())
-    	{
+    	if (slot != null && slot.getHasStack()) {
     		ItemStack itemstack1 = slot.getStack();
     		itemstack = itemstack1.copy();
     		
     		if (index == OUTPUT) {
-    			if (!this.mergeItemStack(itemstack1, OUTPUT+1, OUTPUT+36+1, true))
-    			{
+    			if (!this.mergeItemStack(itemstack1, OUTPUT+1, OUTPUT+36+1, true)) {
     				return null;
     			}
     			slot.onSlotChange(itemstack1, itemstack);
@@ -44,24 +42,21 @@ public class ContainerCombustionGenerator extends MechaTechContainer<TileEntityC
         		if (TileEntityFurnace.isItemFuel(itemstack1)) {
         			if (!this.mergeItemStack(itemstack1, FUEL, FUEL+1, false))
         			{
-        			return null;
+        				return null;
         			}
         		}
         	} else if (index == FUEL) {
-        		if (!this.mergeItemStack(itemstack1, FUEL+1, FUEL+36+1, true))
-    			{
+        		if (!this.mergeItemStack(itemstack1, FUEL+1, FUEL+36+1, true)) {
     				return null;
     			}
     			slot.onSlotChange(itemstack1, itemstack);
         	} else if (index >= OUTPUT+1 && index < OUTPUT+28) {
-        		if (!this.mergeItemStack(itemstack1, OUTPUT+28, OUTPUT+37, false))
-        		{
+        		if (!this.mergeItemStack(itemstack1, OUTPUT+28, OUTPUT+37, false)) {
         			return null;
         		}
         	}
     		
-    		if (itemstack1.stackSize == 0)
-    		{
+    		if (itemstack1.stackSize == 0) {
     			slot.putStack((ItemStack)null);
     		} else {
     			slot.onSlotChanged();
