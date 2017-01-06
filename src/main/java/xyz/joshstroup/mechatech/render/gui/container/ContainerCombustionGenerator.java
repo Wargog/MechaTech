eec474fd4c4296dc60d9213ab80c53061ddf0f44
Joshua Stroup <josh@joshstroup.xyz>
2017-01-06 15:48:31 -0500

    Fix formartting to fit rest of project

--- src/main/java/xyz/joshstroup/mechatech/render/gui/container/ContainerCombustionGenerator.java
+++ src/main/java/xyz/joshstroup/mechatech/render/gui/container/ContainerCombustionGenerator.java
@@ -27,51 +27,66 @@ public class ContainerCombustionGenerator extends MechaTechContainer<TileEntityC
     @Override
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
     	ItemStack itemstack = null;
-    	Slot slot = (Slot)this.inventorySlots.get(index);
+    	Slot slot = (Slot) this.inventorySlots.get(index);
     	
-    	if (slot != null && slot.getHasStack())
+    	if(slot != null && slot.getHasStack())
     	{
     		ItemStack itemstack1 = slot.getStack();
     		itemstack = itemstack1.copy();
     		
-    		if (index == OUTPUT) {
-    			if (!this.mergeItemStack(itemstack1, OUTPUT+1, OUTPUT+36+1, true))
+    		if(index == OUTPUT)
+    		{
+    			if(!this.mergeItemStack(itemstack1, OUTPUT + 1, OUTPUT + 36 + 1, true))
     			{
     				return null;
     			}
+
     			slot.onSlotChange(itemstack1, itemstack);
-    		} else if (index != FUEL) {
-        		if (TileEntityFurnace.isItemFuel(itemstack1)) {
-        			if (!this.mergeItemStack(itemstack1, FUEL, FUEL+1, false))
+    		}
+    		else if(index != FUEL)
+    		{
+        		if(TileEntityFurnace.isItemFuel(itemstack1))
+        		{
+        			if(!this.mergeItemStack(itemstack1, FUEL, FUEL + 1, false))
         			{
-        			return null;
+        				return null;
         			}
         		}
-        	} else if (index == FUEL) {
-        		if (!this.mergeItemStack(itemstack1, FUEL+1, FUEL+36+1, true))
+        	}
+        	else if(index == FUEL)
+        	{
+        		if (!this.mergeItemStack(itemstack1, FUEL + 1, FUEL + 36 +1, true))
     			{
     				return null;
     			}
+
     			slot.onSlotChange(itemstack1, itemstack);
-        	} else if (index >= OUTPUT+1 && index < OUTPUT+28) {
-        		if (!this.mergeItemStack(itemstack1, OUTPUT+28, OUTPUT+37, false))
+        	}
+        	else if(index >= OUTPUT + 1 && index < OUTPUT + 28)
+        	{
+        		if(!this.mergeItemStack(itemstack1, OUTPUT + 28, OUTPUT + 37, false))
         		{
         			return null;
         		}
         	}
     		
-    		if (itemstack1.stackSize == 0)
+    		if(itemstack1.stackSize == 0)
     		{
-    			slot.putStack((ItemStack)null);
-    		} else {
+    			slot.putStack((ItemStack) null);
+    		}
+    		else
+			{
     			slot.onSlotChanged();
     		}
     		
-    		if (itemstack1.stackSize == itemstack.stackSize) {
+    		if(itemstack1.stackSize == itemstack.stackSize)
+    		{
     			return null;
     		}
+
     		slot.onPickupFromSlot(playerIn, itemstack1);
     	}
+
     	return itemstack;
     }
 }
